880b65a0a8bd561d9db5ee609740b5d7669ce43d
MalcolmEggs <matthew.guo@gmail.com>
2017-05-10 19:25:24 -0400

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,12 +1,22 @@
+/**
+ * @author Matthew Guo
+ * Dice class that rolls two dice to get a random integer value
+ */
 public class Dice {
 	 public int die1;
      public int die2;
      
-     public Dice() {
+     /**
+     * Constructor that displays roll results
+     */
+    public Dice() {
          System.out.println("You rolled a " + die1 + " and a " + die2);
      }
      
-     public void roll() {
+     /**
+     * Rolls two dice and sets the values to die1 and die2
+     */
+    public void roll() {
          die1 = (int)(Math.random()*6) + 1;
          die2 = (int)(Math.random()*6) + 1;
          
