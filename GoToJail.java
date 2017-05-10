da7de3a23f545ce918dcbe729bcbc13a557f7124
dtran16 <trandylan8671@students.esuhsd.org>
2017-05-10 16:30:00 -0700

    Update GoToJail.java

--- GoToJail.java
+++ GoToJail.java
@@ -1,10 +1,19 @@
-
+/**
+* Class that represets a go to jail location
+* that sends the player to jail
+*/
 public class GoToJail extends Location{
-
+	/**
+	* Constructor that calls the super construcor to initialize the name
+	*/
 	public GoToJail(String n) {
 		super(n);
 	}
-
+	
+	/**
+	* Sends the player to jail by calling the go to jail method of the player
+	* that sets jail status to true
+	*/
 	@Override
 	public void doAction(Player p) {
 		p.goToJail();
