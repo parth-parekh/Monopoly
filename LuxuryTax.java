9168c4ffe491afe03862ca8eafdea6fe56f00d61
dtran16 <trandylan8671@students.esuhsd.org>
2017-05-10 16:42:19 -0700

    Create LuxuryTax.java

---
+++ LuxuryTax.java
@@ -0,0 +1,18 @@
+/**
+* Class that represents a luxury tax location on the board
+*/
+public class LuxuryTax extends Location {
+  /**
+  * Constructor that initializes the name of the LuxuryTax location
+  */
+  public LuxuryTax (String n){
+    super(n);
+  }
+  /**
+  * Method that subtracts the luxury tax (75) from the player's balance
+  */
+  public void doAction(Player p){
+    p.changeMoney(-75);
+  }
+}
+  
