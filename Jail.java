ff08b4f475ba950b904e8ff09fc5539644ed794c
MalcolmEggs <matthew.guo@gmail.com>
2017-05-10 19:40:19 -0400

    Update Jail.java

--- Jail.java
+++ Jail.java
@@ -1,10 +1,21 @@
 
+/**
+ * @author Matthew Guo, Kaveh Mohebbi, Parth Parekh, Harlene Kaur, Kaitlyn Gunadhi 
+ *  location in which the player is put in if they land on Go To Jail
+ *
+ */
 public class Jail extends Location{
 
+	/**
+	 * @param n name of location
+	 */
 	public Jail(String n) {
 		super(n);
 	}
 
+	/**
+	 * @param p Player that landed on jail
+	 */
 	@Override
 	public void doAction(Player p) {
 		return;
