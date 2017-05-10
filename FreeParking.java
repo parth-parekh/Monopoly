063d2d9f1d368f35d42d35d08ffc36fd0397ca59
MalcolmEggs <matthew.guo@gmail.com>
2017-05-10 19:38:29 -0400

    Update FreeParking.java

--- FreeParking.java
+++ FreeParking.java
@@ -1,13 +1,23 @@
 
+/**
+ * @author Matthew Guo, Kaveh Mohebbi, Parth Parekh, Harlene Kaur, Kaitlyn Gunadhi 
+ * free parking location that gives the player a random amount of money
+ */
 public class FreeParking extends Location{
 
+	/**
+	 * @param n name of location
+	 */
 	public FreeParking(String n) {
 		super(n);
 	}
 
+	/**
+	 * @param p Player that gains a 0-100 dollars, determined randomly
+	 */
 	@Override
 	public void doAction(Player p) {
-		p.changeMoney((int)(Math.random() * 200) - 100);
+		p.changeMoney((int)(Math.random() * 101);
 	}
 
 }
