66f38ab41a98cab68c9c1bffa37f7a505b51c402
kgunadhi <kgunadhi@gmail.com>
2017-05-10 16:55:50 -0700

    Add files via upload

--- ChanceLocation.java
+++ ChanceLocation.java
@@ -1,13 +1,27 @@
 public class ChanceLocation extends Location {
 
+	/**
+	 * Constructor that calls the Location constructor to initialize name
+	 * 
+	 * @param n
+	 *            - name
+	 */
 	public ChanceLocation(String n) {
 		super(n);
 	}
 
+	/**
+	 * Returns random value from -200 to 200
+	 * 
+	 * @return random value
+	 */
 	public int getValue() {
 		return (int) (Math.random() * 401) - 200;
 	}
 
+	/**
+	 * Adds or subtracts value from player's money
+	 */
 	@Override
 	public void doAction(Player p) {
 		int x = getValue();
--- ComLocation.java
+++ ComLocation.java
@@ -1,17 +1,32 @@
 public class ComLocation extends Location {
 
+	/**
+	 * Constructor that calls the Location constructor to initialize name
+	 * 
+	 * @param n
+	 *            - name
+	 */
 	public ComLocation(String n) {
 		super(n);
 	}
 
+	/**
+	 * Returns random value from 0 to 300
+	 * 
+	 * @return random value
+	 */
 	public int getValue() {
-		return (int) (Math.random() * 301) + 100;
+		return (int) (Math.random() * 201) + 100;
 	}
 
+	/**
+	 * Adds value to player's money
+	 */
 	@Override
 	public void doAction(Player p) {
 		int x = getValue();
-		System.out.println("Community Chest: " + p.getName() + " won $" + x + ".");
+		System.out.println("Community Chest: " + p.getName() + " won $" + x
+				+ ".");
 		p.changeMoney(x);
 	}
 
