dfb8666486e4dda4793ce18eb7b90bf926e78dd0
MalcolmEggs <matthew.guo@gmail.com>
2017-05-10 19:46:57 -0400

    Update RailRoad.java

--- RailRoad.java
+++ RailRoad.java
@@ -1,14 +1,22 @@
 
+/**
+ * @author Matthew Guo, Kaveh Mohebbi, Parth Parekh, Harlene Kaur, Kaitlyn Gunadhi 
+ * railroad location that can be bought be players
+ */
 public class RailRoad extends Buyable {
 
+	/** Constructor that sets the name and cost
+	 * @param n name
+	 * @param c cost
+	 */
 	public RailRoad(String n, int c) {
 		super(n, c);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	/**
-	 *
-	 * @return
+	 * returns the rent cost of the railroad
+	 * @return cost
 	 * 
 	 */
 	public int getRent() {
