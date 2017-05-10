19c89dd17183e73c84be6907604f2d31a64fcca9
harlenekaur <harlenekaur10@gmail.com>
2017-05-10 16:43:53 -0700

    Update ChanceLocation.java

--- ChanceLocation.java
+++ ChanceLocation.java
@@ -1,9 +1,16 @@
-
+/**
+* This class represents landing on Chance a 
+* @author Harlene Kaur, Parth Parekh, Matthew Guo, Kaitlyn Gunadhi, Dylan Tran, and Kaveh Mohebbi
+*/
 public class ChanceLocation extends Location {
 
 	public ChanceLocation(String n) {
 		super(n);
 	}
+	
+	/**
+	* @return a value between 0 to 400 after 200 is subtracted from that number 
+	*/
 	public int getValue(){
 		return (int)(Math.random() * 401)  - 200;
 	}
