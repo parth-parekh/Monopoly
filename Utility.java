b773cfedde04e03e83c7143207aa55ce96bfb319
MalcolmEggs <matthew.guo@gmail.com>
2017-05-14 23:08:19 -0700

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -1,19 +1,31 @@
 
+/**
+ * @author Parth Parekh, Matthew Guo, Harlene Kaur, Kaveh Mohebbi, Dylan Tran, Kaitlyn Gunadhi
+ * utility that can be bought by players
+ */
 public class Utility extends Buyable{
 	
+	/** Constructor that sets the name, position, and cost of the buyable
+	 * @param n name
+	 * @param c cost
+	 * @param pos position
+	 */
 	public Utility (String n, int c, int pos){
 		super(n, c, pos);
 	}
 	
 	
+	/** returns the rent cost, which is a random number up to $121
+	 * @return rent cost
+	 */
 	@Override
 	public int getRent(){
 		return (int)(Math.random() * 121);
 	}
 	
 	/**
-	 * 
-	 * @param p - the player who bought the property
+	 * gives ownership to the player who bought the property
+	 * @param p the player who bought the property
 	 * 
 	 */
 	public void buy (Player p) {
