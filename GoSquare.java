0ed831abf4471b0ef586507de3ff76dfe2a89ac4
MalcolmEggs <matthew.guo@gmail.com>
2017-05-14 22:58:55 -0700

    Update GoSquare.java

--- GoSquare.java
+++ GoSquare.java
@@ -1,10 +1,22 @@
 
+/**
+ * @author Parth Parekh, Matthew Guo, Harlene Kaur, Kaveh Mohebbi, Dylan Tran, Kaitlyn Gunadhi
+ * The class is the Go square on the Monopoly board that gives the player $200
+ */
 public class GoSquare extends Location {
 
+	/** Constructor that sets the name and position of the location
+	 * @param n name
+	 * @param pos position
+	 */
 	public GoSquare(String n, int pos) {
 		super(n, pos);
 	}
 
+	/** gives the player that passed it $200
+	 * @param p player
+	 * @return string that states which player passed go and gained $200
+	 */
 	@Override
 	public String doAction(Player p) {
 		p.changeMoney(200);
